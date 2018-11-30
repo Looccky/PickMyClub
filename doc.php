293933def842e4af958d40d5d7fdc257b66160f5
CoralineKARRER <coraline.karrer@epsi.fr>
2018-11-30 17:35:40 +0100

    Update doc.php
    rzgta

--- doc.php
+++ doc.php
@@ -5,7 +5,7 @@ $bdd = new PDO('mysql:host=localhost;dbname=espace_membre', 'root', '' );
 if(isset($_POST['forminscription']))
 {
     if(!empty($_POST['user_name']) AND !empty($_POST['telephone']) AND !empty($_POST['mail']) AND !empty($_POST['mdp']) AND !empty($_POST['confirm']) AND !empty($_POST['user_name']) AND !empty($_POST['pannel'])){
-        
+        echo "ok":
     }
 }
     
